e58765d920880032948e03f95333868b080e993e
Bart van der Plas <30440133+mortuur@users.noreply.github.com>
2024-09-02 14:12:11 +0200

    add controller GameOfLife

---
+++ GameOfLife.php
@@ -0,0 +1,17 @@
+<?php 
+
+class GameOfLifeController
+{
+    public int $x;
+    public int $y;
+
+    public function setup()
+    {
+    }
+    public function safe()
+    {
+    }
+    public function stop()
+    {
+    }
+}
