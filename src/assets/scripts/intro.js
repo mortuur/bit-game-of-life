efbc9d0aa5640aa5c6ad05307bdecf774feae4b8
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-09-04 02:04:09 +0200

    linked intro to index

--- src/assets/scripts/intro.js
+++ src/assets/scripts/intro.js
@@ -1,33 +1,32 @@
 $(function () {
     var welcomeSection = $('.welcome-section'),
         enterButton = $('.enter-button'),
-        targetUrl = 'index.html';  
+        targetUrl = '/index.html';
 
-    
+
     setTimeout(function () {
         welcomeSection.removeClass('content-hidden');
     }, 800);
 
-    
+
     function fadeOutAndRedirect() {
-        welcomeSection.addClass('content-hidden'); 
+        welcomeSection.addClass('content-hidden');
         setTimeout(function () {
-            window.location.href = targetUrl; 
-        }, 1000); 
+            window.location.href = targetUrl;
+        }, 1000);
     }
 
-    
+
     enterButton.on('click', function (e) {
-        e.preventDefault(); 
+        e.preventDefault();
         fadeOutAndRedirect();
     });
 
-    
+
     $(document).on('keydown', function (e) {
         if (e.key === 'Enter') {
-            e.preventDefault(); 
+            e.preventDefault();
             fadeOutAndRedirect();
         }
     });
 });
-
